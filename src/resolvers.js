4066d2ea9e3550e94bf987fec60ebecf875dde63
ruandao <ljy080829@gmail.com>
2021-11-16 14:01:49 +0800

    字段修改

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,7 +44,14 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    durationInSeconds: ({ length }) => {
+      return length
+    }
   },
+  Module: {
+    durationInSeconds: ({length}) => length,
+  }
 };
 
 module.exports = resolvers;
